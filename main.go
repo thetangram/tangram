da192d9b7cb8c5e2b810c804954bb789e4c98678
Jose <jomoespe@gmail.com>
2017-12-29 22:19:36 +0100

    Add healthiness, liveness and readiness checking entrypoints
    Added checking entrypoints. The service listen in 8080, not configurable
    at this point.

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	defaultAddress = ":8080"
 )
 
 var (
@@ -10,9 +16,39 @@ var (
 	buildDate = "unknown"
 )
 
+func healthy() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	}
+}
+
+func ready() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	}
+}
+
+func alive() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	}
+}
+
+func address() string {
+	return defaultAddress
+}
+
 func main() {
-	fmt.Printf("Tangram test\n")
-	fmt.Printf("version:    %s\n", version)
-	fmt.Printf("build:      %s\n", build)
-	fmt.Printf("build date: %s\n", buildDate)
+	fmt.Printf("Tangram\n")
+	fmt.Printf("  version:    %s\n", version)
+	fmt.Printf("  build:      %s\n", build)
+	fmt.Printf("  build date: %s\n", buildDate)
+
+	http.HandleFunc("/healthy", healthy())
+	http.HandleFunc("/ready", ready())
+	http.HandleFunc("/alive", alive())
+	log.Fatal(http.ListenAndServe(address(), nil))
 }
