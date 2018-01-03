93ae691f5c5ce461242472832079b4fdb42b35c9
Jose <jomoespe@gmail.com>
2018-01-03 15:52:28 +0100

    Add ready handler test

--- main_test.go
+++ main_test.go
@@ -21,18 +21,18 @@ func TestHealthyHandler(t *testing.T) {
 /*
  WARNING! To be able to test this feature, the application status probably needs to
         be injected or mocked
-
+*/
 func TestReadyHandler(t *testing.T) {
     handlerSUT := readyHandler
     r, _ := http.NewRequest("GET", "/ready", nil)
-    if err := handlerTester(r, handlerSUT, healthyStatus); err != nil {
-        t.Errorf("healthy handler error %v", err)
+    if err := handlerTester(r, handlerSUT, notReadyStatus); err != nil {
+        t.Errorf("ready handler error %v", err)
     }
-    if err := handlerTester(r, handlerSUT, healthyPayload); err != nil {
-        t.Errorf("healthy handler error %v", err)
+    if err := handlerTester(r, handlerSUT, notReadyPayload); err != nil {
+        t.Errorf("ready handler error %v", err)
     }
 }
-*/
+
 
 func healthyStatus(rr *httptest.ResponseRecorder) (err error) {
 	if status := rr.Code; status != http.StatusOK {
@@ -49,15 +49,15 @@ func healthyPayload(rr *httptest.ResponseRecorder) (err error) {
 	return
 }
 
-func readyStatus(rr *httptest.ResponseRecorder) (err error) {
-	if status := rr.Code; status != http.StatusOK {
+func notReadyStatus(rr *httptest.ResponseRecorder) (err error) {
+	if status := rr.Code; status != http.StatusServiceUnavailable {
 		return fmt.Errorf("healthy handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 	return
 }
 
-func readyPayload(rr *httptest.ResponseRecorder) (err error) {
-	expected := "OK"
+func notReadyPayload(rr *httptest.ResponseRecorder) (err error) {
+	expected := "NO"
 	if rr.Body.String() != expected {
 		return fmt.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
