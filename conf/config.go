d524cd4b6fdf3e432f067b0b78a85f224c1bc247
Jose <jomoespe@gmail.com>
2018-01-05 09:08:13 +0100

    Load conf value from environment variables

--- conf/config.go
+++ conf/config.go
@@ -8,6 +8,10 @@
 //   - default values
 package config
 
+import (
+    "os"
+)
+
 const (
 	defaultConfigFile = "tangram.toml"
 	// defaults
@@ -32,6 +36,19 @@ func Load() (c Config, err error) {
 
 func loadConfig(file string) (c Config, err error) {
 	c = Config{}
-	c.addr = defaultAddress
+	c.addr = confValue("address", "server.address", "ADDRESS", defaultAddress)
 	return
+}
+
+// confValue returns a configuration value from best-fit source.
+// Parameters:
+// - arg: the name of configuration value in the command line argument 
+// - conf: the name of configuration value in configuration file 
+// - env: the name of configuration value in the environment 
+// - def: the default value 
+func confValue(arg string, conf string, env string, def string) string {
+    if value, exist := os.LookupEnv(env); exist {
+        return value
+    }
+    return def
 }
