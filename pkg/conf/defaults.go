013c2679acd2ebfac45a2731e7c0efaca351f224
Jose <jomoespe@gmail.com>
2018-01-12 17:11:59 +0100

    Rename default constants names

--- pkg/conf/defaults.go
+++ pkg/conf/defaults.go
@@ -5,19 +5,19 @@ import (
 )
 
 const (
-	defaultAddress               = ":2018"
-	defaultHTTPReadTimeout       = 500 * time.Millisecond
-	defaultHTTPWriteTimeout      = 2 * time.Second
-	defaultSystemShutdownTimeout = 5 * time.Second
+	defaultAddress         = ":2018"
+	defaultReadTimeout     = 500 * time.Millisecond
+	defaultWriteTimeout    = 2 * time.Second
+	defaultShutdownTimeout = 5 * time.Second
 )
 
 // Defaults creates a conf instance with default values
 func defaults() (c Config, err error) {
 	c = Config{
 		addr:            defaultAddress,
-		readTimeout:     defaultHTTPReadTimeout,
-		writeTimeout:    defaultHTTPWriteTimeout,
-		shutdownTimeout: defaultSystemShutdownTimeout,
+		readTimeout:     defaultReadTimeout,
+		writeTimeout:    defaultWriteTimeout,
+		shutdownTimeout: defaultShutdownTimeout,
 	}
 	return
 }
